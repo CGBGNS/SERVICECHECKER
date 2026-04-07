cb2979e31264e488bb73cac0bf99e480d0fa425b
CGBGNS <136849109+CGBGNS@users.noreply.github.com>
2026-04-07 16:13:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  clientId: '2f0fb504-43b1-4a1b-9166-b2f094dea435',
+  clientId: '8e43d237-60ba-4f7a-b41f-cda36f779cab',
   environment: 'mypurecloud.de',
   redirectUri: 'https://CGBGNS.github.io/SERVICECHECKER/index.html',
   scopes: []
